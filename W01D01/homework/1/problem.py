0296d16db84f07c12eba0c8a310839e9b381a9d3
Billy <billy.phan4@gmail.com>
2024-02-20 22:59:56 -0800

    problem 1 done

--- W01D01/homework/1/problem.py
+++ W01D01/homework/1/problem.py
@@ -1,4 +1,28 @@
 class Solution:
     def groupAnagrams(self, strs):
         # Remove pass and write code here
-        print(strs)
+        hashmap = {}
+        res = []
+        for word in strs:
+            key = self.abc_order(word)
+
+            if key not in hashmap:
+                hashmap[key] = []
+            hashmap[key].append(word)
+
+        # for key in hashmap:
+        #     values = hashmap[key]
+        #     res.append(values)
+        #or res.extend(hashmap.values())
+        # or.... return list(hashmap.values())       
+        #.values create a view object
+        return list(hashmap.values())
+    def abc_order(self,word):
+        word_list = list(word)
+        word_list.sort()
+        new_word ="".join(word_list)
+        return new_word
+
+sample = Solution()
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(sample.groupAnagrams(strs))
