846c27554551eb28ee1de128e92993c297a603f0
Billy <billy.phan4@gmail.com>
2024-02-25 18:04:51 -0800

    2sum done

--- W01D02/homework/1/problem.py
+++ W01D02/homework/1/problem.py
@@ -1,4 +1,11 @@
 class Solution:
     def twoSum(self, nums, target):
         # Remove pass and write code here
-        pass
+        hash = {}
+
+        for i in range(len(nums)):
+            currNum = nums[i]
+            compliment = target - currNum
+            if compliment in hash:
+                return i, hash[compliment]
+            hash[currNum] = i
