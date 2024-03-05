90a9ca2107f186ccc1c42afd5b8b0fc9453d6497
aymak91 <amakyu@gmail.com>
2024-03-04 18:22:48 -0800

    cleanup

--- W03D01/classwork/2/solution.py
+++ W03D01/classwork/2/solution.py
@@ -1,26 +1,27 @@
 class Solution:
     def searchMatrix(self, matrix, target):
 
-        rows = len(matrix)
-        cols = len(matrix[0])
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
 
-        top, bottom = 0, rows-1
+        top, bottom = 0, ROWS-1
 
         while top <= bottom:
             row = top + (bottom-top)//2
 
-            if target > matrix[row][cols-1]:
+            if target > matrix[row][COLS-1]:
                 top = row+1
             elif target < matrix[row][0]:
                 bottom = row-1
             else:
                 break
 
-        if top > bottom:
-            return False
+        print(top, bottom)
+        # if top > bottom:
+        #     return False
 
-        row = top + (bottom-top)//2
-        left, right = 0, cols-1
+        # row = top + (bottom-top)//2
+        left, right = 0, COLS-1
 
 
         while left <= right:
