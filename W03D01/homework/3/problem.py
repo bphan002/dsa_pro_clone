ecf302da7ecb0aabff9bccacbcc3040809c69bf3
Billy <billy.phan4@gmail.com>
2024-03-10 15:08:35 -0700

    homework greedy

--- W03D01/homework/3/problem.py
+++ W03D01/homework/3/problem.py
@@ -1,4 +1,24 @@
+import math
+
 class Solution:
     def minEatingSpeed(self, piles, h):
         # Remove pass and write code here
-        pass
+        left = 1
+        right = max(piles)
+        mins = float('inf')
+
+        while left <= right:
+            mid = left + (right - left)//2
+            time = 0
+
+            for pile in piles:
+                time += math.ceil(pile/mid)
+                print(math.ceil(pile/mid))
+         
+            if time <= h:
+                mins = min(mins, mid)
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return mins
